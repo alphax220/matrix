0769089e4c92ffc513a4c9c4b6f9f96012da9f41
Alpha Diallo <DRAGON VERT@DESKTOP-DE9PHME>
2022-03-09 19:33:28 +0000

    scale text and power-off

--- main.lua
+++ main.lua
@@ -7,6 +7,16 @@ local with = love.graphics.getWidth()
 
 local timer = 1
 
+local text = {}
+text.mat = "MATRIX"
+text.scaleX = 2
+text.scaleY = 2
+text.x = love.graphics.getWidth()/2 -200
+text.y = love.graphics.getHeight()/2 - 100
+text.alpha = 0
+text.increase = false
+text.green = 1
+
 --Create Data
 function DataCreate()
   local data = {}
@@ -25,6 +35,24 @@ end
 
 function love.update()
   
+  
+  --Increase scale Text
+  if text.increase == false then
+  text.scaleX = text.scaleX + 0.01
+  text.scaleY = text.scaleY + 0.01
+  text.alpha = text.alpha + 0.001
+  end
+  
+  if text.alpha > 1 then
+    text.alpha = 1
+  end
+  
+  if text.scaleX > 10 then
+    text.increase = true
+    text.green = 0
+    love.quit()
+  end
+
   --Generate Data
   timer = timer - 10
   if timer < 0 then
@@ -42,14 +70,18 @@ end
 
 function love.draw()
 
-love.graphics.print(tostring(#allData),100,100)
+ love.graphics.setColor(0,text.green,0,text.alpha)
+ love.graphics.print(text.mat,text.x,text.y,0,text.scaleX,text.scaleY)
+ love.graphics.reset()
+
 for fall, data in ipairs(allData) do
   
   data.y = data.y + 5
-  love.graphics.setColor(0,1,0,data.alpha)
+  love.graphics.setColor(0,text.green,0,data.alpha)
   love.graphics.print(tostring(data.number),data.x,data.y,0,data.scale,data.scale)
   love.graphics.reset()
   
 end
 
 end
+
